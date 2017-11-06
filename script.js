d0c08265c279bca67b02e130e7feda9a9a6a1d82
unknown <satish.dawala@ADMINISTRATOR74.ojasit.in>
2017-11-06 12:14:18 +0530

    added media queries using bootstrap and mobile and dektop devices

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ var app = angular.module('myApp',[]);
 app.controller('myCtrl', ['$scope', function($scope){
 
 	$scope.firstname = "satish kumar";
+	$scope.tablets = ["Ipad","Samsung","Dell","Asus","Lenovo"];
+	$scope.smarts = ["Iphone","Samsung","Redmi","Moto","Lenovo"];
+	$scope.tablet = $scope.tablets[0];
+	$scope.smart = $scope.smarts[0];
 	
 }])
